30430d439a5baf59c0706b70b68e1ef3044fe6db
lijinxi <1767270730@qq.com>
2016-08-24 19:31:59 +0800

    增加JSON省份和城市MODEL

---
+++ src/main/java/com/hc/scm/uc/dao/file/json/model/City.java
@@ -0,0 +1,25 @@
+package com.hc.scm.uc.dao.file.json.model;
+
+public class City {
+    private   int  id;
+    private  String cityNo;
+    private  String cityName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCityNo() {
+		return cityNo;
+	}
+	public void setCityNo(String cityNo) {
+		this.cityNo = cityNo;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+}
---
+++ src/main/java/com/hc/scm/uc/dao/file/json/model/Province.java
@@ -0,0 +1,25 @@
+package com.hc.scm.uc.dao.file.json.model;
+
+public class Province {
+   private   int  id;
+   private  String provinceNo;
+   private  String provinceName;
+   public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProvinceNo() {
+		return provinceNo;
+	}
+	public void setProvinceNo(String provinceNo) {
+		this.provinceNo = provinceNo;
+	}
+	public String getProvinceName() {
+		return provinceName;
+	}
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+}
